a4994eec11b94d914ec6edb51bda78425cf5bc21
Sauvageduck24 <74764572+Sauvageduck24@users.noreply.github.com>
2021-09-08 17:54:48 +0200

    Update main.py

--- main.py
+++ main.py
@@ -78,7 +78,7 @@ if check1:
     epochs=st.slider('Pasos',1,500,key='3')
     prediction=st.button('Hacer predicción',key='4')
     if prediction:
-        prediction,test_data, predicted_prices,prediction,loss=get_prediction.start(company=selected_stock,period='7wk',prediction_days=hour, target=target,interval='1h', epochs=epochs,test_period='1h',interval_period='1h')
+        prediction,test_data, predicted_prices,prediction,loss=get_prediction.start(company=selected_stock,period='7wk',prediction_days=hour, target=target,interval='1h', epochs=epochs,test_period='2h',interval_period='1h')
         #plot(test_data,target)
         st.write(f'Predicción: {prediction}')
         st.write(f'Pérdida en el entrenamiento: {loss}')
@@ -93,7 +93,7 @@ if check2:
     epochs2=st.slider('Pasos',1,500,key='8')
     prediction2=st.button('Hacer predicción',key='9')
     if prediction2:
-        prediction,test_data, predicted_prices,prediction,loss=get_prediction.start(company=selected_stock,period='7mo',prediction_days=hour2, target=target2,interval='1d', epochs=epochs2,test_period='1d',interval_period='1d')
+        prediction,test_data, predicted_prices,prediction,loss=get_prediction.start(company=selected_stock,period='7mo',prediction_days=hour2, target=target2,interval='1d', epochs=epochs2,test_period='2d',interval_period='1d')
         #plot(test_data,target2)
         st.write(f'Predicción: {prediction}')
         st.write(f'Pérdida en el entrenamiento: {loss}')
@@ -109,7 +109,7 @@ if check3:
     prediction3=st.button('Hacer predicción',key='14')
     hour3=hour3*5
     if prediction3:
-        prediction,test_data, predicted_prices,prediction,loss=get_prediction.start(company=selected_stock,period='7mo',prediction_days=hour3, target=target3,interval='1d', epochs=epochs3, test_period='1d', interval_period='1d')
+        prediction,test_data, predicted_prices,prediction,loss=get_prediction.start(company=selected_stock,period='7mo',prediction_days=hour3, target=target3,interval='1d', epochs=epochs3, test_period='2d', interval_period='1d')
         #plot(test_data,target3)
         st.write(f'Predicción: {prediction}')
         st.write(f'Pérdida en el entrenamiento: {loss}')
